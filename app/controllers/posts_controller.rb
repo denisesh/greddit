45bab78b7453a73324526eea95640e1b97a26e8f
denisesh <denisesh@gmail.com>
2015-01-02 14:50:40 -0800

    users and posts wired up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,26 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
 
+  def show
+  	@post = Post.find(params[:id])
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+   	
+
+  private
+
+  def post_params
+  	params.require(:post).permit(
+  	  :title,
+  	  :description,
+  	  :user_id,
+  	  )
+  	
+  end
 end
